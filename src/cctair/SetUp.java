3961a8a5c56950fde4c1ff20c74cd50d0fe1398b
Johnes <johnescamargo@gmail.com>
2019-10-25 21:34:52 +0100

    fridayNight

--- src/cctair/SetUp.java
+++ src/cctair/SetUp.java
@@ -1,5 +1,6 @@
 package cctair;
 
+import java.util.Date;
 import java.util.Random;
 
 public class SetUp {
@@ -9,19 +10,26 @@ public class SetUp {
     String[] surnames = {" Jones", " O'Camargo", " O'Silva", " Gerrard", " Gate", " Drinkwater", " Python", " Vimm"};
 
     String[] makes = {"Embraer", "Boing", "Airbus"};
-    String[] model = {"707", "860", "870", "890", "830", "353", "160", "855"};
-    int[] capacity = {300, 400, 350, 200, 250};
+    String[] model = {"707", "860", "870", "890", "830", "353", "160", "855", "700"};
+    int[] capacity = {300, 400, 350, 200, 250, 400};
     int[] rank = {1, 2, 3};
 
-    String origin;
-    String destination;
-    String departure;
-    String arrival;
-    String dateOfFlight;
-    Airplane[] airplane = new Airplane[1];
-    Pilot[] pilot = new Pilot[1];
-    
+    String[] origin = {"Dublin", "Porto", "Paris", "Sao Paulo",
+        "Tokio", "Liverpool", "Manchester", "Rome", "New York", "Florida", "Lisbon", "London", "Rio de Janeiro"};
+    String[] destination = {"Dublin", "Porto", "Paris", "Sao Paulo",
+        "Tokio", "Liverpool", "Manchester", "Rome", "New York", "Florida", "Lisbon", "London", "Rio de Janeiro"};
+    String[] departure = {"10:30","11:30","12:30","13:30","14:30","15:30","16:30",};
+    String[] arrival = {"17:30","18:30","19:30","20:30","21:30","22:30","23:30","00:30"};
+    Date dateOfFlight;
+    //Airplane[] airplane = new Airplane[1];
+    //Pilot[] pilot = new Pilot[1];
 
+    //    • origin (e.g. “Dublin”) 
+//    • destination (e.g. “New York”) 
+//    • departure time (e.g. “10:10”)   ****
+//    • arrival time (e.g “13:30”)         ****
+//    • date of flight (e.g. “05/12/2015”) 
+//    • aircraft assigned (this should be an object); 
     Random rGen = new Random();
 
     public Airplane[] setAircrafts(Airplane[] airplane, Pilot[] pilot) {
@@ -54,7 +62,7 @@ public class SetUp {
 
     public Flight[] setFlights(Flight[] flight) {
 
-        for (int i = 0; i < flight.length; i++) {
+        for (int i = 0; i <= 10; i++) {
 
         }
         return flight;
